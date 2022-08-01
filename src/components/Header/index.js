7ba553f72f13cca26c769d7b4e79d4642899fcaa
yakupkalayci <ykpklyc18@gmail.com>
2022-08-01 19:57:21 +0300

    desing header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import "./styles.css";
+
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <img src='billgates.jpg' className='headerImg' alt='Bill Gates'/>
+        <h1>Spend Bill Gates' Money</h1>
+    </header>
   )
 }
 
